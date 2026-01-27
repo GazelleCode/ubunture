8196e3b1b5fdd5e756d9a6c818c4f5cf5f6c2d2c
Natsuki Hino <ntk.shrike@gmail.com>
2026-01-27 19:03:07 +0900

    maintext translation

--- assets/js/translations.js
+++ assets/js/translations.js
@@ -15,7 +15,13 @@ const translations = {
     "header.donation": "Make a Donation",
     "hero.title": "Environment",
     "hero.subtitle": "Provide us a green and healthy life<br> we need to protect",
-    "hero.btn": "Join Us Now"
+    "hero.btn": "Join Us Now",
+    "mission.title": "Our Mission",
+    "mission.heading": "Our main goal is to protect animals",
+    "mission.text1": "The automated process starts as soon as your clothes go into the machine. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.",
+    "mission.text2": "Interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.",
+    "mission.ceo_role": "CEO, Consulto",
+    "mission.ceo_name": "Capcilena Hanry"
   },
   
   // 日本語
@@ -33,7 +39,13 @@ const translations = {
     "header.donation": "寄付をする",
     "hero.title": "環境保護",
     "hero.subtitle": "守るべき緑豊かで健康的な生活を、私たちに。",
-    "hero.btn": "今すぐ参加"
+    "hero.btn": "今すぐ参加",
+    "mission.title": "私たちの使命",
+    "mission.heading": "私たちの主な目標は動物を保護することです",
+    "mission.text1": "自動化されたプロセスは、衣服が機械に入るとすぐに始まります。Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.",
+    "mission.text2": "Interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.",
+    "mission.ceo_role": "CEO, Consulto",
+    "mission.ceo_name": "Capcilena Hanry"
   }
   // (例) 将来フランス語を足す場合、ここに追加するだけでOK
   // fr: {
